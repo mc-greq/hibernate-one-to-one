e0874f4e4e6acb340b395f0d15e70751629c2680
mc-greq <greq19@gmail.com>
2019-01-31 20:33:27 +0100

    Handling one-to-one relation with Hibernate

---
+++ src/example/nio/entities/InstructorDetail.java
@@ -0,0 +1,61 @@
+package example.nio.entities;
+
+import javax.persistence.*;
+
+// annotate the class as an entity and map it to database table
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+
+    // define the fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    // create constructors
+    public InstructorDetail() {
+    }
+
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+    // generate getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    // override toString()
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "id=" + id +
+                ", youtubeChannel='" + youtubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
